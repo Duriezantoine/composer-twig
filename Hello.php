8af2efa36b26333ce50dc03426b56d1df0c116e9
Duriez-antoine <79853001+Duriezantoine@users.noreply.github.com>
2021-04-12 07:14:10 +0200

    Add files via upload

---
+++ Hello.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Wcs; 
+//pOUR pouvoir prendre la classe avec auto load 
+
+class Hello
+{
+    public function talk() :string
+    {
+        return "helloworld" ;
+    }
+}
---
+++ installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'antoine/twigcomposer2',
+  ),
+  'versions' => 
+  array (
+    'antoine/twigcomposer2' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
